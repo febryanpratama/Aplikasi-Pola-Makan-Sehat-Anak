71b2c5906bf7bc9f0c4c12987363b0bb480faa8d
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-06-29 17:25:52 +0700

    feat: rev logo

--- resources/views/layouts/navigation/guest.blade.php
+++ resources/views/layouts/navigation/guest.blade.php
@@ -2,8 +2,8 @@
      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           
           <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="{{ asset('assets/logo-1.png') }}" class="h-8" alt="Flowbite Logo">
           <img src="{{ asset('assets/logo-2.jpg') }}" class="h-8" alt="Flowbite Logo">
+          <img src="{{ asset('assets/logo-1.png') }}" class="h-8" alt="Flowbite Logo">
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Puskesmas Rawat Jalan Mempawah Hilir</span>
           </a>
 
