bc6b9ee45e00fc2b464cfa84df70a8f755586e79
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-06-29 18:42:50 +0700

    feat: rev all data

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex">
-    <img src="{{ asset('assets/logo-2.jpg') }}" class="w-10">
-    <img src="{{ asset('assets/logo-1.png') }}" class="w-20" alt="">
+    <img src="{{ asset('assets/logo-2.jpg') }}" class="w-20">
+    <img src="{{ asset('assets/logo-1.png') }}" class="w-10" alt="">
 </div>
