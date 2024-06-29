9678b706539b9e46e4cfc1ac65833be6e6f25d4f
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-06-29 17:38:53 +0700

    feat: rev page food hide

--- resources/views/pages/admin/food/index.blade.php
+++ resources/views/pages/admin/food/index.blade.php
@@ -81,9 +81,9 @@
                                                             {{ __('Ubah') }}
                                                        </a>
                                                        
-                                                       <a href="#" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                                       {{-- <a href="#" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                                        {{ __('Lihat Tampilan') }}
-                                                       </a>
+                                                       </a> --}}
 
                                                        <div id="popup-modal-{{ $item->slug }}" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                                             <div class="relative p-4 w-full max-w-md max-h-full">
